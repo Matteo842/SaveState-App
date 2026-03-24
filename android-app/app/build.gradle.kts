747a83bbc64c3a5c4a60565b17b35e4142e8d1b8
Matteo Cioffi <37482462+Matteo842@users.noreply.github.com>
2026-03-24 17:22:20 +0100

    Update build.gradle.kts

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.savestate.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.4"
+        versionCode = 4
+        versionName = "0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
